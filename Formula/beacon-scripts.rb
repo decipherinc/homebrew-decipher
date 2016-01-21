3d6a4c4f62fea7febd819c7230283ca168234d8a
Christopher Hiller <chiller@decipherinc.com>
2016-01-21 01:38:37 -0800

    add version for scripts

--- Formula/beacon-scripts.rb
+++ Formula/beacon-scripts.rb
@@ -1,6 +1,7 @@
 class BeaconScripts < Formula
   desc "Global scripts for Beacon to be used on host OS"
-
+  version "0.0.0"
+  
   url "file://#{Dir.pwd}/scripts/here", :using => :curl
 
   def install
