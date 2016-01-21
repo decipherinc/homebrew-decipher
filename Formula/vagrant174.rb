776e35afc672aecb8f439a8ae33cf614050b2ca5
Christopher Hiller <chiller@decipherinc.com>
2016-01-21 01:27:10 -0800

    rename stuff

--- Formula/vagrant174.rb
+++ Formula/vagrant174.rb
@@ -1,4 +1,4 @@
-cask 'vagrant1_7' do
+cask 'vagrant174' do
   version '1.7.4'
   sha256 '3d2e680cc206ac1d480726052e42e193eabce56ed65fc79b91bc85e4c7d2deb8'
 
--- Formula/virtualbox4332.rb
+++ Formula/virtualbox4332.rb
@@ -1,4 +1,4 @@
-cask 'virtualbox4_3' do
+cask 'virtualbox4332' do
   version '4.3.32-103443'
   sha256 'dcfbd1d3014ab393dc5944a9474eeabf8b33471e7d95cb4c94070dc7acab772c'
 
