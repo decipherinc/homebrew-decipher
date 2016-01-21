0b639acd81d3d7ee6c5d4bc9e4b6a831e183452d
Christopher Hiller <chiller@decipherinc.com>
2016-01-21 02:17:27 -0800

    try alternate dl strategy

--- Formula/beacon-scripts.rb
+++ Formula/beacon-scripts.rb
@@ -1,10 +1,29 @@
+class CopyStrategy < AbstractFileDownloadStrategy
+
+  def cached_location
+    HOMEBREW_CACHE.join(@url)
+  end
+
+  def fetch
+    ohai "Copying #{@url}"
+
+    unless File.exist?(@url)
+      raise CopyStrategyError.new(@url)
+    end
+
+    FileUtils.cp(@url, cached_location)
+  end
+
+end
+
 class BeaconScripts < Formula
   desc "Global scripts for Beacon to be used on host OS"
   version "0.0.0"
-  
-  url "file://#{Dir.pwd}/scripts/here", :using => :curl
+
+  url "./scripts/here", :using => CopyStrategy
 
   def install
     prefix.install "here"
+    bin.install_symlink "here"
   end
 end
