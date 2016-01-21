bbfe154bf561001f0db941a2cba53eeb18170ceb
Christopher Hiller <chiller@decipherinc.com>
2016-01-21 01:36:56 -0800

    fix url in beacon-scripts?

--- Formula/beacon-scripts.rb
+++ Formula/beacon-scripts.rb
@@ -1,8 +1,7 @@
 class BeaconScripts < Formula
   desc "Global scripts for Beacon to be used on host OS"
-  resource "here" do
-    url "file://#{Dir.pwd}/scripts/here", :using => :curl
-  end
+
+  url "file://#{Dir.pwd}/scripts/here", :using => :curl
 
   def install
     prefix.install "here"
